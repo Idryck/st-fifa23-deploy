d526efdbdde859db7bd4c5bbd5d68932010c0a38
Idryck <lucassouza5527@gmail.com>
2025-02-23 17:13:37 -0300

    load
    assim todas as paginas poderão carregar o cache, não importa de onde vc entre

--- pages/2_jogadores.py
+++ pages/2_jogadores.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+
+
+if 'data' not in  st.session_state:
+    df_data=pd.read_csv('CLEAN_FIFA23_official_data.csv', index_col=0)
+    df_data=df_data[df_data['Contract Valid Until'] >= datetime.today().year]
+    df_data=df_data[df_data['Value(£)']>0]
+    df_data=df_data.sort_values(by='Overall', ascending=False)
+    st.session_state['data']=df_data
+    
+    
+    
+    
 md=st.markdown
 st.set_page_config(layout='wide', page_title='FIFA23')
 #sidebar
